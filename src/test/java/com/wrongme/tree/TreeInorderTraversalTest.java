809a5fef611a07b16a72999c8cd3964d42d15d06
WRong <wrongvest@163.com>
2020-11-08 22:48:21 +0800

    fix: 修改单元测试工具

--- src/test/java/com/wrongme/tree/TreeInorderTraversalTest.java
+++ src/test/java/com/wrongme/tree/TreeInorderTraversalTest.java
@@ -1,6 +1,7 @@
 package com.wrongme.tree;
 
 import com.wrongme.basic.TreeNode;
+import com.wrongme.utils.BinaryTreeUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,19 +14,7 @@ public class TreeInorderTraversalTest {
 
     @Before
     public void init() {
-        root = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node4 = new TreeNode(4);
-        TreeNode node5 = new TreeNode(5);
-        TreeNode node6 = new TreeNode(6);
-        TreeNode node7 = new TreeNode(7);
-        root.left = node2;
-        root.right = node3;
-        node2.left = node4;
-        node2.right = node5;
-        node5.left = node7;
-        node3.left = node6;
+        root = BinaryTreeUtils.commonBinaryTree();
     }
 
     @Test
