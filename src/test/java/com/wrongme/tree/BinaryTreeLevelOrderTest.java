867e4a057e42ee739f813522fa422c3faf9a02bc
WRong <wrongvest@163.com>
2020-11-08 22:47:01 +0800

    feat: 增加单元测试工具

--- src/test/java/com/wrongme/tree/BinaryTreeLevelOrderTest.java
+++ src/test/java/com/wrongme/tree/BinaryTreeLevelOrderTest.java
@@ -1,6 +1,7 @@
 package com.wrongme.tree;
 
 import com.wrongme.basic.TreeNode;
+import com.wrongme.utils.BinaryTreeUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,19 +12,7 @@ public class BinaryTreeLevelOrderTest {
 
     @Before
     public void init() {
-        root = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node4 = new TreeNode(4);
-        TreeNode node5 = new TreeNode(5);
-        TreeNode node6 = new TreeNode(6);
-        TreeNode node7 = new TreeNode(7);
-        root.left = node2;
-        root.right = node3;
-        node2.left = node4;
-        node2.right = node5;
-        node5.left = node7;
-        node3.left = node6;
+        root = BinaryTreeUtils.commonBinaryTree();
     }
 
     @Test
---
+++ src/test/java/com/wrongme/utils/BinaryTreeUtils.java
@@ -0,0 +1,101 @@
+package com.wrongme.utils;
+
+import com.wrongme.basic.TreeNode;
+
+public class BinaryTreeUtils {
+
+    /**
+     * 满二叉树
+     * 1
+     * \     \
+     * 2        3
+     * \    \   \   \
+     * 4    5   6    7
+     *
+     * @return
+     */
+    public static TreeNode fullBinaryTree() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        return root;
+    }
+
+    /**
+     * 普通树
+     * 1
+     * \     \
+     * 2      3
+     * \    \   \
+     * 4    5   6
+     * \
+     * 7
+     *
+     * @return
+     */
+    public static TreeNode commonBinaryTree() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node5.left = node7;
+        node3.left = node6;
+        return root;
+    }
+
+    /**
+     * 只有左子树
+     * 1
+     * \
+     * 2
+     * \
+     * 3
+     *
+     * @return
+     */
+    public static TreeNode leftBinaryTree() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        root.left = node2;
+        node2.left = node3;
+        return root;
+    }
+
+
+    /**
+     * 只有右子树
+     * 1
+     * \
+     * 2
+     * \
+     * 3
+     *
+     * @return
+     */
+    public static TreeNode rightBinaryTree() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        root.right = node2;
+        node2.right = node3;
+        return root;
+    }
+}
