7f2aa23767f4da2b844f0d61a9e50f67e08e2f95
WRong <wrongvest@163.com>
2020-11-08 21:27:13 +0800

    feat:第 102 题：层序遍历

---
+++ src/main/java/com/wrongme/tree/BinaryTreeLevelOrder.java
@@ -0,0 +1,45 @@
+package com.wrongme.tree;
+
+import com.wrongme.basic.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author wrong
+ * 102. 二叉树的层序遍历
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+ * @date 2020年11月8日20:56:03
+ */
+public class BinaryTreeLevelOrder {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // 每一层的个数 2^(n-1)
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        // 压入队尾
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int currentLevelSize = queue.size();
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                // 弹出队首
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ret.add(level);
+        }
+
+        return ret;
+    }
+}
