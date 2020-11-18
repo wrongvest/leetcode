1b5ec68cc18d3858dfd2681ca6a0f9f0b1f20ebd
WRong <wrongvest@163.com>
2020-11-18 21:50:10 +0800

    feat:105. 从前序与中序遍历序列构造二叉树-迭代

--- src/main/java/com/wrongme/tree/BinaryTreeBuildTree.java
+++ src/main/java/com/wrongme/tree/BinaryTreeBuildTree.java
@@ -2,7 +2,9 @@ package com.wrongme.tree;
 
 import com.wrongme.basic.TreeNode;
 
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 /**
@@ -48,4 +50,38 @@ public class BinaryTreeBuildTree {
                 inorderRoot+1,inorder_right);
         return root;
     }
+
+    /**
+     * 迭代
+     * @param preorder
+     * @param inorder
+     * @return
+     */
+    public TreeNode buildTree2(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.push(root);
+        int inorderIndex = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+
+
 }
