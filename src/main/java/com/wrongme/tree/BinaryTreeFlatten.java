df6efc94bf7a5fc3018373a7f54bfef34679f596
WRong <wrongvest@163.com>
2021-03-13 14:23:57 +0800

    114. 二叉树展开为链表-2

--- src/main/java/com/wrongme/tree/BinaryTreeFlatten.java
+++ src/main/java/com/wrongme/tree/BinaryTreeFlatten.java
@@ -3,6 +3,8 @@ package com.wrongme.tree;
 import com.wrongme.basic.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -30,4 +32,28 @@ public class BinaryTreeFlatten {
         preOrderTraversal(root.left);
         preOrderTraversal(root.right);
     }
+
+
+    // 方法二
+    public void flatten2(TreeNode root) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                list.add(node);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+        }
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
 }
