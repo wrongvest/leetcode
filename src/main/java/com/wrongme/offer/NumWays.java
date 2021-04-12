c83b82ee5c805db4750983e92439acba9305f161
WRong <wrongvest@163.com>
2021-04-12 21:20:52 +0800

    剑指 Offer 10- II. 青蛙跳台阶问题

---
+++ src/main/java/com/wrongme/offer/NumWays.java
@@ -0,0 +1,16 @@
+package com.wrongme.offer;
+
+public class NumWays {
+
+    public int numWays(int n) {
+        int a = 1, b = 1, sum;
+        for(int i = 0; i < n; i++){
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+
+}
