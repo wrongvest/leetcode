1318801d5f98a9171b015e00e5baad1d88ac0171
WRong <wrongvest@163.com>
2021-06-29 20:31:13 +0800

    168. Excel表列名称

---
+++ src/main/java/com/wrongme/bfs/LadderLengthSolution.java
@@ -0,0 +1,16 @@
+package com.wrongme.bfs;
+
+import java.util.List;
+
+/**
+ * 127. 单词接龙
+ */
+public class LadderLengthSolution {
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)){
+            return 0;
+        }
+        return 0;
+    }
+}
