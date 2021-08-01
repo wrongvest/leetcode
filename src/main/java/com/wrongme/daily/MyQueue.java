ab452581f60bcbd3be3b4def10e413ff5758e005
WRong <wrongvest@163.com>
2021-08-01 12:30:44 +0800

    232. 用栈实现队列

---
+++ src/main/java/com/wrongme/daily/MyQueue.java
@@ -0,0 +1,75 @@
+package com.wrongme.daily;
+
+import java.util.Stack;
+
+public class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        if (stack2 != null) {
+            while (!stack2.isEmpty()) {
+                stack1.push(stack2.pop());
+            }
+            stack1.push(x);
+        } else {
+            stack1.push(x);
+        }
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (!stack2.isEmpty()) {
+            return stack2.pop();
+        } else {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
+        }
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (!stack2.isEmpty()) {
+            return stack2.peek();
+        } else {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+            return stack2.peek();
+        }
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return stack2.isEmpty() && stack1.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        MyQueue obj = new MyQueue();
+        obj.push(1);
+        obj.push(2);
+        int param_2 = obj.pop();
+        int param_3 = obj.peek();
+        boolean param_4 = obj.empty();
+    }
+
+}
