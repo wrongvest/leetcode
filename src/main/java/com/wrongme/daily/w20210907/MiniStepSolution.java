ea04b9ad6f0249d5240f5029edb910ab312150de
WRong <wrongvest@163.com>
2021-09-19 20:08:43 +0800

    w20210907-20210919

---
+++ src/main/java/com/wrongme/daily/w20210907/MiniStepSolution.java
@@ -0,0 +1,29 @@
+package com.wrongme.daily.w20210907;
+
+public class MiniStepSolution {
+    int INF = 0x3f3f3f3f;
+
+    public int minSteps(int n) {
+        int f[][] = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                f[i][j] = INF;
+            }
+        }
+        f[1][0] = 0;
+        f[1][1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int min = INF;
+            for (int j = 0; j <= i / 2; j++) {
+                f[i][j] = f[i - 1][j]+1;
+                min = Math.min(f[i][j], min);
+            }
+            f[i][i] = min + 1;
+        }
+        int ans = INF;
+        for (int i = 0; i <=n; i++) {
+            ans = Math.min(ans,f[n][i]);
+        }
+        return ans;
+    }
+}
