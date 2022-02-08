4b96d76eb9337695603e7ddbaa0752d5ee45ded0
WRong <wrongvest@163.com>
2022-02-08 21:36:29 +0800

    1001. 网格照明

--- src/main/java/com/wrongme/daily/w20220207/SolutionTuesday.java
+++ src/main/java/com/wrongme/daily/w20220207/SolutionTuesday.java
@@ -1,41 +1,80 @@
 package com.wrongme.daily.w20220207;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
- * 1405. 最长快乐字符串
+ * 1001. 网格照明
  */
 public class SolutionTuesday {
-    // 优先使用数量最多的字符 来拼凑字符串，当数量最多的与已拼凑的字符串后两位相同时，我们应该
-    // 使用数量次多的字符 来拼凑字符串，依次往复，最终可以得到我们的结果。
-    public String longestDiverseString(int a, int b, int c) {
-        int[][] arr = new int[][]{{'a', a}, {'b', b}, {'c', c}};
-        StringBuilder sb = new StringBuilder(a + b + c);
-        while (true) {
-            Arrays.sort(arr, (x, y) -> y[1] - x[1]);
-            int n = sb.length();
-            // 如果数量最多的为0了，跳出循环
-            if (arr[0][1] == 0) {
-                break;
-            }
-            if (n >= 2 && sb.charAt(n - 1) == arr[0][0] && sb.charAt(n - 2) == arr[0][0]) {
-                if (arr[1][1] == 0) {
-                    break;
-                }
-                sb.append((char) arr[1][0]);
-                arr[1][1]--;
-            } else {
-                // 拼接数量最多的，同时其数量减1
-                sb.append((char) arr[0][0]);
-                arr[0][1]--;
-            }
+    int[][] dirs = new int[][]{{0, 0}, {0, -1}, {0, 1}, {-1, 0}, {-1, -1}, {-1, 1}, {1, 0}, {1, -1}, {1, 1}};
 
+    public int[] gridIllumination(int n, int[][] lamps, int[][] queries) {
+        long N = n;
+        Map<Integer, Integer> row = new HashMap<>();
+        Map<Integer, Integer> col = new HashMap<>();
+        Map<Integer, Integer> left = new HashMap<>();
+        Map<Integer, Integer> right = new HashMap<>();
+        Set<Long> set = new HashSet<>();
+        for (int[] lamp : lamps) {
+            int x = lamp[0];
+            int y = lamp[1];
+            int a = x + y;
+            int b = x - y;
+            if (set.contains(x * N + y)) {
+                continue;
+            }
+            increment(row, x);
+            increment(col, y);
+            increment(left, a);
+            increment(right, b);
+            set.add(x * N + y);
         }
-        return sb.toString();
+        int m = queries.length;
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            int[] q = queries[i];
+            int x = q[0];
+            int y = q[1];
+            int a = x + y;
+            int b = x - y;
+            if (row.containsKey(x) || col.containsKey(y) || left.containsKey(a) || right.containsKey(b)) {
+                ans[i] = 1;
+            }
+            for (int[] d : dirs) {
+                int nx = x + d[0];
+                int ny = y + d[1];
+                int na = nx + ny;
+                int nb = nx - ny;
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                    continue;
+                }
+                if (set.contains(nx * N + ny)) {
+                    set.remove(nx * N + ny);
+                    decrement(row, nx);
+                    decrement(col, ny);
+                    decrement(left, na);
+                    decrement(right, nb);
+                }
+            }
+        }
+        return ans;
+    }
+
+    private void decrement(Map<Integer, Integer> map, int key) {
+        if (map.get(key) == 1) {
+            map.remove(key);
+        } else {
+            map.put(key, map.get(key) - 1);
+        }
+    }
+
+    private void increment(Map<Integer, Integer> map, int key) {
+        map.put(key, map.getOrDefault(key, 0) + 1);
     }
 
     public static void main(String[] args) {
-        SolutionTuesday tuesday = new SolutionTuesday();
-        System.out.println(tuesday.longestDiverseString(1, 1, 7));
     }
 }
