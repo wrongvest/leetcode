81f2ce7d5290448e06aa140bccd5b4e2f5593b93
WRong <wrongvest@163.com>
2022-02-12 23:32:48 +0800

    1447. 最简分数

---
+++ src/main/java/com/wrongme/daily/w20220207/SolutionFriday.java
@@ -0,0 +1,14 @@
+package com.wrongme.daily.w20220207;
+
+import java.util.Arrays;
+
+public class SolutionFriday {
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = nums[k-1] - nums[0];
+        for (int i = k; i < nums.length; i++) {
+            ans = Math.min(ans,nums[i]-nums[i-k+1]);
+        }
+        return ans;
+    }
+}
