28e5cf103934b87c786221729a21323afb06c38a
WRong <wrongvest@163.com>
2022-02-18 21:42:36 +0800

    1791. 找出星型图的中心节点

---
+++ src/main/java/com/wrongme/daily/w20220214/FridaySolution.java
@@ -0,0 +1,15 @@
+package com.wrongme.daily.w20220214;
+
+/**
+ * 1791. 找出星型图的中心节点
+ */
+public class FridaySolution {
+    public int findCenter(int[][] edges) {
+        int a = edges[0][0];
+        int b = edges[0][1];
+        if (a == edges[1][0] || a == edges[1][1]) {
+            return a;
+        }
+        return b;
+    }
+}
